1384440b50cd59228d3d40580c3bcd398e8c3c3e
ash-uncover <ash.uncover@gmail.com>
2022-09-25 07:01:32 +0200

    basic compo display

--- src/components/compos/Compos.jsx
+++ src/components/compos/Compos.jsx
@@ -1,12 +1,56 @@
 import React from 'react'
 
-const Compos = () => {
+import {
+  useDispatch,
+  useSelector,
+  useState,
+} from 'lib/hooks'
 
+import {
+  selectors as DataSelectors,
+} from 'store/data'
+
+import './_compos.scss'
+
+const Compos = () => {
+  // Hooks
+  const compos = useSelector(DataSelectors.dataComposSelector)
+
+  // Rendering
   return (
-    <div className='app-content'>
-      COMPOS
+    <div className='app-content compos'>
+      {compos.map(compo => <Compo key={compo.name} compo={compo} />)}
     </div>
   )
 }
+
+const Compo = ({ compo }) => {
+  // Hooks
+
+  // Rendering
+  return (
+    <div className='compo'>
+      {compo.name}
+      <div className='compo-champions'>
+        {compo.champions.map(id => <CompoChampion key={id} id={id} />)}
+      </div>
+    </div>
+  )
+}
+
+const CompoChampion = ({ id }) => {
+  // Hooks
+  const champion = useSelector(DataSelectors.dataChampionSelector(id))
+
+  // Rendering
+  return (
+    <div className='compo-champion'>
+      <div className='compo-champion-image'>
+        <img src={`images/champions/${champion.id}.jpg`} />
+      </div>
+    </div>
+  )
+}
+
 
 export default Compos
